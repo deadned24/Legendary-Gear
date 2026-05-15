8af09bccaa43889042f09e0a56a0e186fb9dc748
deadned24 <deadned24@outlook.com>
2026-05-15 16:03:12 -0700

    Refactor comments and update weapon description
    Removed old comments and updated the description for the legendary wand.

--- relay/item.12223.js
+++ relay/item.12223.js
@@ -1,4 +1,4 @@
-//legendary seal-clubbing club, diablo stylized
+//legendary pasta wand, diablo stylized
 
 // load a CSS file instead of writing a CSS file in JS
 const link = document.createElement('link');
@@ -9,9 +9,3 @@ document.getElementsByTagName('HEAD')[0].appendChild(link);
 
 //page specfic fixes
 document.querySelector("#description > blockquote:nth-child(3) > b:nth-child(3)").innerHTML = "weapon (1-handed legendary wand)";
-
-const blueFonts = document.querySelectorAll("font[color='blue']");
-    blueFonts.forEach(font => {
-        font.removeAttribute("color"); // Strip the old blue attribute
-        font.style.setProperty("color", "rgb(144, 52, 255)"); // Apply "Diablo purple"
-    });
